b6dd39996bcf7146382ef40d915bd1ae0a4b3145
N7ghtm4r3 <manuel033689@gmail.com>
2022-04-16 12:09:50 +0200

    Load first file

--- src/main/java/com/tecknobit/apimanager/Manager/APIRequest.java
+++ src/main/java/com/tecknobit/apimanager/Manager/APIRequest.java
@@ -1,13 +1,21 @@
 package com.tecknobit.apimanager.Manager;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
+
 public class APIRequest {
 
     private HttpURLConnection httpURLConnection;
+    private String errorResponse;
     private int requestTimeout;
 
     public APIRequest() {
@@ -15,6 +23,10 @@ public class APIRequest {
     }
 
     public APIRequest(int requestTimeout) {
+        this.requestTimeout = requestTimeout;
+    }
+
+    public void setRequestTimeout(int requestTimeout) {
         this.requestTimeout = requestTimeout;
     }
 
@@ -42,8 +54,46 @@ public class APIRequest {
         httpURLConnection.setConnectTimeout(requestTimeout);
     }
 
-    public void setRequestTimeout(int requestTimeout) {
-        this.requestTimeout = requestTimeout;
+    public String getRequestResponse() throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader;
+        String line;
+        boolean isInError = false;
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+        } catch (IOException e) {
+            bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+            isInError = true;
+        }
+        while ((line = bufferedReader.readLine()) != null)
+            stringBuilder.append(line);
+        if(isInError) {
+            errorResponse = stringBuilder.toString();
+            return errorResponse;
+        }
+        return stringBuilder.toString();
+    }
+
+    /** Method to get params signature of an HTTP request
+     * @param #key: secret key used to signature request
+     * @param #data: data to sign
+     * @return signature es. c8db66725ae71d6d79447319e617115f4a920f5agcdabcb2838bd6b712b053c4"
+     * **/
+    public String getSignature(String key, String data) throws Exception {
+        Mac sha256 = Mac.getInstance("HmacSHA256");
+        sha256.init(new SecretKeySpec(key.getBytes(UTF_8), "HmacSHA256"));
+        return encodeHexString(sha256.doFinal(data.replace("?","").getBytes(UTF_8)));
+    }
+
+    public String assembleAdditionalParams(String mandatoryParams, HashMap<String, String> extraParams){
+        StringBuilder params = new StringBuilder(mandatoryParams);
+        for (String key : extraParams.keySet())
+            params.append("&").append(key).append("=").append(extraParams.get(key));
+        return params.toString();
+    }
+
+    public String getErrorResponse() {
+        return errorResponse;
     }
 
 }
