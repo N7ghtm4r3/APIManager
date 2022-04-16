b41403c2cda70b376b9fceab29ac24f5e9ec0cc9
N7ghtm4r3 <manuel033689@gmail.com>
2022-04-16 15:21:54 +0200

    Load first test version APIManager

--- src/main/java/com/tecknobit/apimanager/Tools/Trading/TradingTools.java
+++ src/main/java/com/tecknobit/apimanager/Tools/Trading/TradingTools.java
@@ -39,9 +39,41 @@ public class TradingTools {
         return roundValue((((lastValue * 100) / startValue) - 100), decimalDigits);
     }
 
+    /** Method to get percent between two values and textualize it
+     * @param #startValue: first value to make compare
+     * @param #lastValue: last value to compare and get percent by first value
+     * @return percent value es. +8% or -8% as {@link String}
+     * **/
+    public String textualizeAssetPercent(double startValue, double lastValue){
+        return textualizeAssetPercent(computeAssetPercent(startValue, lastValue));
+    }
+
+    /** Method to get percent between two values and textualize it
+     * @param #startValue: first value to make compare
+     * @param #lastValue: last value to compare and get percent by first value
+     * @param #decimalDigits: number of digits to round final percent value
+     * @return percent value es. +8% or -8% as {@link String}
+     * **/
+    public String textualizeAssetPercent(double startValue, double lastValue, int decimalDigits){
+        return textualizeAssetPercent(computeAssetPercent(startValue, lastValue, decimalDigits));
+    }
+
+    /** Method to format percent between two values and textualize it
+     * @param #percent: value to format
+     * @return percent value formatted es. +8% or -8% as {@link String}
+     * **/
+    public String textualizeAssetPercent(double percent){
+        if(percent > 0)
+            return "+" + percent + "%";
+        else if(percent < 0)
+            return "-" + percent + "%";
+        else
+            return "=" + percent + "%";
+    }
+
     /** Method to round a value
      * @param #value: value to round
-     * @param #decimalDigits: number of digits to round final percent value
+     * @param #decimalDigits: number of digits to round final value
      * @return value rounded with decimalDigits inserted
      * @throws IllegalArgumentException if decimalDigits is negative
      * **/
@@ -51,11 +83,13 @@ public class TradingTools {
         return parseDouble(format("%."+decimalDigits+"f", value));
     }
 
-    /** Method to round a value
-     * @param #historicalValues: value to round
-     * @param #lastValue: number of digits to round final percent value
-     * @return value rounded with decimalDigits inserted
-     * @throws IllegalArgumentException if decimalDigits is negative
+    /** Method to get prevision of an asset in base of days's gap inserted
+     * @param #historicalValues: previous values of asset used to compute prevision
+     * @param #lastValue: last value of the asset to compare and fetch prevision
+     * @param #intervalDays: days gap for the prevision range
+     * @param #offsetRange: tolerance for select similar value compared to lastValue inserted
+     * @return prevision value as a double es. 8 or -8
+     * @throws IllegalArgumentException if lastValue is negative or intervalDays are less or equal to 0
      * **/
     public double computeTPTOPAsset(ArrayList<Double> historicalValues, double lastValue, int intervalDays, int offsetRange){
         if(lastValue < 0)
@@ -65,6 +99,28 @@ public class TradingTools {
         return computeTPTOPAsset(historicalValues.toArray(new Double[historicalValues.size()]),lastValue,intervalDays,offsetRange);
     }
 
+    /** Method to get prevision of an asset in base of days's gap inserted and round it
+     * @param #historicalValues: previous values of asset used to compute prevision
+     * @param #lastValue: last value of the asset to compare and fetch prevision
+     * @param #intervalDays: days gap for the prevision range
+     * @param #offsetRange: tolerance for select similar value compared to lastValue inserted
+     * @param #decimalDigits: number of digits to round final prevision value
+     * @return prevision value as a double es. 8 or -8
+     * @throws IllegalArgumentException if lastValue is negative or intervalDays are less or equal to 0
+     * **/
+    public double computeTPTOPAsset(ArrayList<Double> historicalValues, double lastValue, int intervalDays, int offsetRange,
+                                    int decimalDigits){
+        return roundValue(computeTPTOPAsset(historicalValues,lastValue, intervalDays,offsetRange), decimalDigits);
+    }
+
+    /** Method to get prevision of an asset in base of days's gap inserted
+     * @param #historicalValues: previous values of asset used to compute prevision
+     * @param #lastValue: last value of the asset to compare and fetch prevision
+     * @param #intervalDays: days gap for the prevision range
+     * @param #offsetRange: tolerance for select similar value compared to lastValue inserted
+     * @return prevision value as a double es. 8 or -8
+     * @throws IllegalArgumentException if lastValue is negative or intervalDays are less or equal to 0
+     * **/
     public double computeTPTOPAsset(Double[] historicalValues, double lastValue, int intervalDays, int offsetRange){
         if(lastValue < 0)
             throw new IllegalArgumentException("lastValue must be positive");
@@ -87,5 +143,18 @@ public class TradingTools {
         return sumTrends/trends.length;
     }
 
+    /** Method to get prevision of an asset in base of days's gap inserted and round it
+     * @param #historicalValues: previous values of asset used to compute prevision
+     * @param #lastValue: last value of the asset to compare and fetch prevision
+     * @param #intervalDays: days gap for the prevision range
+     * @param #offsetRange: tolerance for select similar value compared to lastValue inserted
+     * @param #decimalDigits: number of digits to round final prevision value
+     * @return prevision value as a double es. 8 or -8
+     * @throws IllegalArgumentException if lastValue is negative or intervalDays are less or equal to 0
+     * **/
+    public double computeTPTOPAsset(Double[] historicalValues, double lastValue, int intervalDays, int offsetRange,
+                                    int decimalDigits){
+        return roundValue(computeTPTOPAsset(historicalValues, lastValue, intervalDays,offsetRange), decimalDigits);
+    }
 
 }
