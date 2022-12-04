4418939265419cf0bdb0396ce19c2ae93419155c
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2022-12-04 21:46:50 +0100

    Added SocketManager API class

--- src/main/java/com/tecknobit/apimanager/apis/SocketManager.java
+++ src/main/java/com/tecknobit/apimanager/apis/SocketManager.java
@@ -6,11 +6,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SocketManager {
 
+    private final ConcurrentHashMap<Integer, ServerSocket> listeners;
     private final boolean serverUse;
     private final boolean allowMultipleListeners;
     private ServerSocket serverSocket;
@@ -19,7 +21,6 @@ public class SocketManager {
     private Runnable currentRoutine;
     private Socket socket;
     private String currentHost;
-    private PrintWriter printWriter;
 
     //CLIENT
     public SocketManager(String host, int serverPort) {
@@ -27,14 +28,18 @@ public class SocketManager {
         currentHost = host;
         currentServerPort = serverPort;
         allowMultipleListeners = false;
+        listeners = null;
     }
 
     //SERVER
     public SocketManager(boolean allowMultipleListeners) {
         serverUse = true;
         this.allowMultipleListeners = allowMultipleListeners;
-        if (allowMultipleListeners)
+        if (allowMultipleListeners) {
             executor = Executors.newCachedThreadPool();
+            listeners = new ConcurrentHashMap<>();
+        } else
+            listeners = null;
     }
 
     public void restartListener(Runnable routine) throws IOException {
@@ -48,10 +53,17 @@ public class SocketManager {
     public void startListener(int port, Runnable routine) throws IOException {
         if (serverUse) {
             if (serverSocket == null || currentServerPort != port) {
-                serverSocket = new ServerSocket(port);
-                currentServerPort = port;
+                if (allowMultipleListeners) {
+                    listeners.put(port, new ServerSocket(port));
+                } else {
+                    serverSocket = new ServerSocket(port);
+                    currentServerPort = port;
+                }
             }
             if (!allowMultipleListeners) {
+                // TODO: 04/12/2022 WORK WHEN IS NOT ALLOWED MULTIPLE LISTENERS TO AVOID MORE COROUTINES AT THE SAME TIME
+                if (executor != null)
+                    executor.shutdown();
                 executor = Executors.newSingleThreadExecutor();
             }
             currentRoutine = routine;
@@ -60,23 +72,42 @@ public class SocketManager {
     }
 
     public Socket acceptRequest() throws IOException {
-        if (serverUse)
+        return acceptRequest(currentServerPort);
+    }
+
+    public Socket acceptRequest(int port) throws IOException {
+        if (serverUse) {
+            if (allowMultipleListeners) {
+                if (listeners.size() == 1)
+                    port = listeners.keys().nextElement();
+                serverSocket = listeners.get(port);
+            }
             return socket = serverSocket.accept();
+        }
         return null;
     }
 
     public synchronized <T> void writeContent(T content) throws IOException {
-        if (!serverUse)
-            socket = new Socket(currentHost, currentServerPort);
-        printWriter = new PrintWriter(socket.getOutputStream(), true);
-        printWriter.println(content.toString());
+        writeContentTo(currentHost, currentServerPort, content);
     }
 
-    public synchronized <T> void writeContent(Socket targetSocket, T content) throws IOException {
-        if (serverUse) {
-            printWriter = new PrintWriter(targetSocket.getOutputStream(), true);
-            printWriter.println(content.toString());
-        }
+    public synchronized <T> void writeContentTo(int port, T content) throws IOException {
+        writeContentTo(currentHost, port, content);
+    }
+
+    public synchronized <T> void writeContentTo(String host, T content) throws IOException {
+        writeContentTo(host, currentServerPort, content);
+    }
+
+    public synchronized <T> void writeContentTo(String host, int port, T content) throws IOException {
+        if (!serverUse)
+            socket = new Socket(host, port);
+        writeContentTo(socket, content);
+    }
+
+    public synchronized <T> void writeContentTo(Socket targetSocket, T content) throws IOException {
+        PrintWriter printWriter = new PrintWriter(targetSocket.getOutputStream(), true);
+        printWriter.println(content.toString());
     }
 
     public synchronized String readContent() throws IOException {
